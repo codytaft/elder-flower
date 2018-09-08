c183ae8b15068c9bc3f39e94ba366216f725e880
Cody Taft <cody.taft@gmail.com>
2018-09-08 16:01:03 -0600

    Create Home files

---
+++ src/containers/Home/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
+import MainContainer from '../MainContainer/MainContainer';
+
+export class Home extends Component {
+  logout = () => {};
+  render() {
+    return (
+      <section>
+        <header>
+          <p>Date</p>
+          <h1>Hi There</h1>
+          <Link
+            exact
+            to="/"
+            className="nav-link nav-link-logout"
+            onClick={this.logout}
+          >
+            Logout
+          </Link>
+        </header>
+        <Navigation />
+        <MainContainer />
+      </section>
+    );
+  }
+}
