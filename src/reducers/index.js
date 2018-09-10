86bfe6dfe9a2c3ec15f118a226d353b37792232f
Cody Taft <cody.taft@gmail.com>
2018-09-10 17:48:11 -0600

    Rearrange file structure

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import { userReducer } from './userReducer';
 
 const rootReducer = combineReducers({
-  currentUser: userReducer
+  currentUser: userReducer,
+  users: userReducer
 });
 
 export default rootReducer;
