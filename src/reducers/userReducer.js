66726c38abf9edda19f223959abc1ce908a0c1f3
Cody Taft <cody.taft@gmail.com>
2018-09-10 14:35:34 -0600

    Setup initial reducers

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,8 +1,10 @@
-export const userReducer = (state = null, action) => {
+export const userReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
       return action.user;
-    case 'CREATE_USER':
+    case 'CREATE_ELDER':
+      return action.user;
+    case 'CREATE_CARER':
       return action.user;
     default:
       return state;
