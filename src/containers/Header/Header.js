9a4fb29ee7be190602353ca48d1218eccd67898e
Cody Taft <cody.taft@gmail.com>
2018-09-12 07:13:20 -0600

    Begin styling header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { setCurrentUser } from '../../actions';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import './Header.css';
 
 export class Header extends Component {
   logoutUser = () => {
@@ -11,13 +12,18 @@ export class Header extends Component {
     this.props.history.replace('/');
   };
   render() {
+    // console.log(this.props.currentUser);
     return (
-      <section className="main-header">
-        <h1>Welcome to Elder Flower</h1>
+      <section className="main-header-section">
+        <h1 className="main-header-title">Welcome to Elder Flower</h1>
         <Link
           exact
           to="/"
-          className="nav-link nav-link-logout"
+          className={
+            !this.props.currentUser
+              ? 'nav-link-logout-hidden'
+              : 'nav-link nav-link-logout'
+          }
           onClick={this.logoutUser}
         >
           Logout
@@ -27,13 +33,17 @@ export class Header extends Component {
   }
 }
 
+export const mapStateToProps = state => ({
+  currentUser: state.currentUser
+});
+
 export const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(Header)
 );
