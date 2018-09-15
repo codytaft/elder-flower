85140a7d0874b4c2db1e07715edb180229930248
Cody Taft <cody.taft@gmail.com>
2018-09-15 12:11:53 -0600

    Fix main container button width

--- src/containers/MainContainer/MainContainer.js
+++ src/containers/MainContainer/MainContainer.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './MainContainer.css';
 export class MainContainer extends Component {
+  constructor() {
+    super(),
+      (this.state = {
+        isClicked: false
+      });
+  }
   render() {
     return (
       <section className="nav-link-section">
-        <NavLink exact to="/login" className="nav-link nav-link-login">
-          Login
+        <NavLink exact to="/login" className="nav-link nav-link-login" onClick>
+          LOGIN
         </NavLink>
         <NavLink exact to="/sign-up-home" className="nav-link nav-link-signup">
-          SignUp
+          SIGNUP
         </NavLink>
       </section>
     );
