c065872441c507cc5737a1a3f8ef294c1f4fbc1d
Cody Taft <cody.taft@gmail.com>
2018-09-15 18:26:01 -0600

    Separate currentUser and users reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,5 @@
 export const userReducer = (state = null, action) => {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
-      return action.user;
     case 'CREATE_ELDER':
       return action.user;
     case 'CREATE_CARER':
