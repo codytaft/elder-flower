fd8e0d780688d8088fa2d593189207ad75c79be4
Cody Taft <cody.taft@gmail.com>
2018-09-16 16:34:44 -0600

    Add conditional rendering for app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import Routes from '../../components/Routes/Routes';
 import Header from '../Header/Header';
 import MainContainer from '../MainContainer/MainContainer';
@@ -8,21 +8,26 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const { location } = this.props;
     return (
-      <div>
-        <Router>
-          <div className="app">
-            <header className="app-header">
-              <Header />
-            </header>
-            <main className="main-container">
-              <MainContainer />
-              <Routes />
-            </main>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <div
+          className={
+            location.pathname === '/sign-up-elder' || '/sign-up-carer'
+              ? 'app-hidden app'
+              : 'app'
+          }
+        >
+          <header className="app-header">
+            <Header />
+          </header>
+          <main className="main-container">
+            <MainContainer />
+            <Routes />
+          </main>
+        </div>
+      </Router>
     );
   }
 }
-export default App;
+export default withRouter(App);
