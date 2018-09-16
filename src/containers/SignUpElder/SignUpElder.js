37875e6d4d317e6cc91797d65ed7443f27b154bc
Cody Taft <cody.taft@gmail.com>
2018-09-16 11:35:04 -0600

    Fix phone number section in SignUpElder

--- src/containers/SignUpElder/SignUpElder.js
+++ src/containers/SignUpElder/SignUpElder.js
@@ -61,6 +61,7 @@ export class SignUpElder extends Component {
             value={this.state.phoneNumber}
             name="phoneNumber"
             placeholder="Phone Number"
+            style={{ marginLeft: '0px' }}
           />
           <button onClick={this.testPhoneNumber} className="phone-test-btn">
             Test
