a6a800f6281fb8f86fff757b52e1c49a66cdfd35
Cody Taft <cody.taft@gmail.com>
2018-09-17 15:12:26 -0600

    Add ecnrypted password to signUpCarer

--- src/containers/SignUpCarer/SignUpCarer.js
+++ src/containers/SignUpCarer/SignUpCarer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import bcrypt from 'bcryptjs';
 import { setCurrentUser } from '../../actions';
 import { withRouter } from 'react-router-dom';
 import {
@@ -18,6 +19,7 @@ export class SignUpCarer extends Component {
       lastName: '',
       phoneNumber: '',
       email: '',
+      password: '',
       contactName: '',
       contactPhone: '',
       isElder: false
@@ -29,11 +31,13 @@ export class SignUpCarer extends Component {
     this.setState({ [name]: value });
   };
 
-  handleClick = e => {
+  handleSubmit = async e => {
     e.preventDefault();
     const phoneNumber = `+1${this.state.phoneNumber.replace(/[- ._]/g, '')}`;
     const contactPhone = `+1${this.state.contactPhone.replace(/[- ._]/g, '')}`;
-    this.setState({ phoneNumber, contactPhone });
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(this.state.password, salt);
+    await this.setState({ phoneNumber, contactPhone, password: hash });
     this.props.setCurrentUser(this.state);
     createUser(this.state);
     const location = { pathname: './dashboard' };
@@ -50,19 +54,36 @@ export class SignUpCarer extends Component {
   };
 
   render() {
+    const {
+      firstName,
+      lastName,
+      email,
+      password,
+      phoneNumber,
+      contactName,
+      contactPhone
+    } = this.state;
+    const isEnabled =
+      firstName.length > 0 &&
+      lastName.length > 0 &&
+      phoneNumber.length > 0 &&
+      email.length &&
+      password.length > 0 &&
+      contactName.length > 0 &&
+      contactPhone.length > 0;
     return (
       <form className="signup-form">
         <input
           onChange={this.handleChange}
           className="signup-first-name signup-input"
-          value={this.state.firstName}
+          value={firstName}
           name="firstName"
           placeholder="First Name"
         />
         <input
           onChange={this.handleChange}
           className="signup-last-name signup-input"
-          value={this.state.lastName}
+          value={lastName}
           name="lastName"
           placeholder="Last Name"
         />
@@ -70,38 +91,53 @@ export class SignUpCarer extends Component {
         <input
           onChange={this.handleChange}
           className="signup-email-address signup-input"
-          value={this.state.email}
+          value={email}
           name="email"
           placeholder="Email Address"
+        />
+        <input
+          onChange={this.handleChange}
+          className="signup-password signup-input"
+          value={password}
+          name="password"
+          placeholder="Password"
         />
         <section className="signup-phone-section">
           <input
             onChange={this.handleChange}
             className="signup-phoneNumber signup-input"
-            value={this.state.phoneNumber}
+            value={phoneNumber}
             name="phoneNumber"
             placeholder="Phone Number"
             style={{ marginLeft: '0px' }}
           />
-          <button onClick={this.testPhoneNumber} className="phone-test-btn">
+          <button
+            onClick={this.testPhoneNumber}
+            className="phone-test-btn"
+            isDisabled={!(phoneNumber.length > 0)}
+          >
             Test
           </button>
         </section>
         <input
           onChange={this.handleChange}
           className="signup-elder-name signup-input"
-          value={this.state.contactName}
+          value={contactName}
           name="contactName"
           placeholder="Elder Contact Name"
         />
         <input
           onChange={this.handleChange}
           className="signup-elder-phone signup-input"
-          value={this.state.contactPhone}
+          value={contactPhone}
           name="contactPhone"
           placeholder="Elder Contact Phone"
         />
-        <button className="signup-submit-btn" onClick={this.handleClick}>
+        <button
+          className="signup-submit-btn"
+          onClick={this.handleSubmit}
+          disabled={!isEnabled}
+        >
           Submit
         </button>
       </form>
