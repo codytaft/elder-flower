dfd3280195015e03fffc5552b7a4e0b27e2a89ed
Cody Taft <cody.taft@gmail.com>
2018-09-18 13:22:50 -0600

    Add calendar to dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -10,11 +10,26 @@ import Header from '../Header/Header';
 import './Dashboard.css';
 export class Dashboard extends Component {
   render() {
+    const { location } = this.props;
     return (
-      <section className="navigation-bar">
-        <Navigation />
-        <MainContainer />
-      </section>
+      <div className="dashboard-wrapper">
+        <section className="navigation-bar">
+          <Navigation />
+        </section>
+        {location.pathname === '/dashboard' ? (
+          <iframe
+            src="https://calendar.google.com/calendar/b/0/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=cody.taft%40gmail.com&amp;color=%2309d753&amp;ctz=America%2FDenver"
+            // style={{border-width:0}}
+            width="800"
+            height="600"
+            frameBorder="0"
+            scrolling="no"
+            className="dashboard-calendar"
+          />
+        ) : (
+          <div />
+        )}
+      </div>
     );
   }
 }
