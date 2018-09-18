2eba641df86e77419b75647a13cc9ba80bc0ef06
Cody Taft <cody.taft@gmail.com>
2018-09-17 19:23:40 -0600

    Test app location change

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -10,13 +10,31 @@ describe('App', () => {
   });
   it('should match the snapshot if location.pathname equals sign-up-elder', () => {
     let wrapper;
-    const mockLocation = { pathname: '/sign-up-elder' };
+    const mockLocation = {
+      pathname: '/sign-up-elder',
+      search: '',
+      hash: ''
+    };
     wrapper = shallow(<App location={mockLocation} />);
     expect(wrapper).toMatchSnapshot();
   });
-  it('should match the snapshot if location.pathname does not equal sign-up-elder', () => {
+  it('should match the snapshot if location.pathname equals sign-up-carer', () => {
     let wrapper;
-    const mockLocation = { pathname: '/' };
+    const mockLocation = {
+      pathname: '/sign-up-carer',
+      search: '',
+      hash: ''
+    };
+    wrapper = shallow(<App location={mockLocation} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+  it('should match the snapshot if location.pathname does not equal sign-up-elder or sign-up-carer', () => {
+    let wrapper;
+    const mockLocation = {
+      pathname: '/home',
+      search: '',
+      hash: ''
+    };
     wrapper = shallow(<App location={mockLocation} />);
     expect(wrapper).toMatchSnapshot();
   });
