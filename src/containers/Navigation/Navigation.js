176f5e29b6e63c75e406104eb1240a1b315e2ef2
Cody Taft <cody.taft@gmail.com>
2018-09-17 19:41:57 -0600

    Begin navigation tests

--- src/containers/Navigation/Navigation.js
+++ src/containers/Navigation/Navigation.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const Navigation = {
-  render() {
-    return (
-      <section className="nav-link-section">
-        <NavLink exact to="/contacts" className="nav-link nav-link-contacts">
-          Contacts
-        </NavLink>
-        <NavLink exact to="/emergency" className="nav-link nav-link-emergency">
-          Emergency
-        </NavLink>
-      </section>
-    );
-  }
+export const Navigation = () => {
+  return (
+    <section className="nav-link-section">
+      <NavLink exact to="/contacts" className="nav-link nav-link-contacts">
+        Contacts
+      </NavLink>
+      <NavLink exact to="/emergency" className="nav-link nav-link-emergency">
+        Emergency
+      </NavLink>
+    </section>
+  );
 };
 
 export default Navigation;
