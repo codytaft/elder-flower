2142d18820736da85193f0bc34bbfef19909b2aa
Cody Taft <cody.taft@gmail.com>
2018-09-18 22:26:07 -0600

    Fix styling on dashboard

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link, withRouter } from 'react-router-dom';
-import Navigation from '../Navigation/Navigation';
-import MainContainer from '../../components/MainContainer/MainContainer';
+import { withRouter } from 'react-router-dom';
+import Navigation from '../../components/Navigation/Navigation';
 import { connect } from 'react-redux';
 import { setCurrentUser } from '../../actions';
-import Header from '../Header/Header';
 
 import './Dashboard.css';
 export class Dashboard extends Component {
@@ -19,7 +17,6 @@ export class Dashboard extends Component {
         {location.pathname === '/dashboard' ? (
           <iframe
             src="https://calendar.google.com/calendar/b/0/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showCalendars=0&amp;showTz=0&amp;mode=WEEK&amp;height=600&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=cody.taft%40gmail.com&amp;color=%2309d753&amp;ctz=America%2FDenver"
-            // style={{border-width:0}}
             width="800"
             height="600"
             frameBorder="0"
@@ -36,7 +33,8 @@ export class Dashboard extends Component {
 
 Dashboard.propTypes = {
   setCurrentUser: PropTypes.func,
-  currentUser: PropTypes.object
+  currentUser: PropTypes.object,
+  location: PropTypes.object
 };
 
 export const mapStateToProps = state => ({
