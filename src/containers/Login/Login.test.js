6e4e6ea44a00f63c76fa43466588ee9a3e4b2ed8
Cody Taft <cody.taft@gmail.com>
2018-09-18 20:29:25 -0600

    Test login

--- src/containers/Login/Login.test.js
+++ src/containers/Login/Login.test.js
@@ -1,14 +1,61 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Login from './Login';
+import { Login } from './Login';
 import { setCurrentUser } from '../../actions';
 import { mapStateToProps, mapDispatchToProps } from './Login';
+import bcrypt from 'bcryptjs';
+
+jest.mock('bcryptjs', () => ({
+  genSaltSync: () => 'sdkfj',
+  hashSync: () => 'yournewhash',
+  compareSync: () => true
+}));
 
 describe('Login', () => {
+  let wrapper;
   it('should match the snapshot', () => {
-    let wrapper;
     wrapper = shallow(<Login />);
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should update state on change', () => {
+    wrapper = shallow(<Login />);
+
+    const mockEmailEvent = {
+      target: { name: 'email', value: 'cody.taft@gmail.com' }
+    };
+    const mockPasswordEvent = {
+      target: { name: 'password', value: '123' }
+    };
+
+    wrapper.instance().handleChange(mockEmailEvent);
+    expect(wrapper.state('email')).toEqual(mockEmailEvent.target.value);
+    wrapper.instance().handleChange(mockPasswordEvent);
+    expect(wrapper.state('password')).toEqual(mockPasswordEvent.target.value);
+  });
+
+  it.skip('should get user on submit', async () => {
+    let mockEvent = { preventDefault: () => jest.fn() };
+    let mockUser = {
+      email: 'cody.taft@gmail.com',
+      password: '123'
+    };
+    let history = {
+      length: 27,
+      action: 'PUSH',
+      location: { pathname: '/dashboard', search: '', hash: '', key: 'kzlimn' }
+    };
+    const email = 'cody.taft@gmail.com';
+    const password = '123';
+    await wrapper.setState({ email, password });
+    await wrapper.instance().handleSubmit(mockEvent);
+    window.fetch = jest.fn().mockImplementation(() =>
+      Promise.resolve({
+        json: () => Promise.resolve(mockUser)
+      })
+    );
+
+    wrapper = shallow(<Login />);
   });
 });
 
